01b9cf462f6acbc842e996fd88e5cbdeca70834a
Brendon de Beer <91954705+monaco-lux@users.noreply.github.com>
2021-11-06 15:27:32 +0200

    Adding HTML boilerplate and form elements

--- Index.php
+++ Index.php
@@ -1,6 +1,23 @@
 <?php
-
-
-
+session_start(); //Initialize Session
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hotel Booking Form</title>
+  <!--  <link rel="stylesheet" href="style.css"> -->
+  </head>
+  <body>
+
+    <form action="newtodo.php" method="post">
+  		<input type="text" name="name" placeholder="What is your name?">
+      <input type="text" name="surname" placeholder="What is your surname?">
+       
+    </form>
+
+  </body>
+</html>
