350fb015490357e40f60acb8842c56513ccde6fb
Brendon de Beer <91954705+monaco-lux@users.noreply.github.com>
2021-11-06 21:16:44 +0200

    Laying ground work for writing the form to a bookings json to read back

--- code/newBooking.php
+++ code/newBooking.php
@@ -1,88 +1,33 @@
 <?php
-session_start(); //Initialize Session
+
+$name = $_POST['name'];
+$surname = $_POST['surname'];
+$email = $_POST['email'];
+$checkIn = $_POST['checkIn'];
+$checkOut = $_POST['checkOut'];
+$hotel = $_POST['hotel'];
+
+$name = trim($name);
+$surname = trim($surname);
+$email = trim($email);
+$checkIn = trim($checkIn);
+$checkOut = trim($checkOut);
+$hotel = trim($hotel);
+
+if($name)
+{
+  if(file_exists('Location: assets/booking.json'))
+  {
+    $bookingJson = file_get_contents('Location: code/booking.json');
+    $bookingJson = json_decode($bookingJson, true);
+  } else {
+    {
+      $bookingJson = [];
+    }
+  }
+  file_put_contents('Location: code/booking.json', json_encode($bookingJson, JSON_PRETTY_PRINT));
+}
+
+header
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hotel Booking Form</title>
-    <link rel="stylesheet" href="code/stylesheet.css">
-  </head>
-  <body>
-    <script src="/code/scripts.js"></script>
-    <h1>Hotel Booking Form</h1>
-    <hr>
-    <form action="newBooking.php" method="post">
-      <div class="group1">
-        <label for="name">What is your name?</label>
-  		   <input
-          type="text"
-          name="name" id="name"
-          placeholder="Joe"
-          autofocus required
-        >
-      </div>
-      <br>
-      <div class="group1">
-        <label for="surname">What is your surname?</label>
-        <input
-          type="text"
-          name="surname" id="surname"
-          placeholder="Blogs"
-          required
-        >
-      </div>
-      <br>
-      <div class="group1">
-        <label for="email">What is your email?</label>
-        <input
-          type="email"
-          name="email" id="email"
-          placeholder="joeblogs@emaildomain.com"
-          required
-        >
-      </div>
-      <br>
-      <div class="group1-inline">
-        <label for="checkIn">Check-in:</label>
-        <input
-          type="date"
-          name="checkIn" id="checkIn"
-          required
-        >
-      </div>
-      <div class="group1-inline">
-        <label for="checkOut">Check-out:</label>
-        <input
-          type="date"
-          name="checkOut" id="checkOut"
-          required
-        >
-      </div>
-      <br>
-      <br>
-      <br>
-      <br>
-      <div class="group1">
-        <label for="hotel">Hotel:</label>
-        <select
-          name="hotel" id="hotel"
-          required
-        >
-          <option value="empty">Choose a hotel</option>
-          <option value="marriot">Marriot</option>
-          <option value="hilton">Hilton</option>
-          <option value="parkIn">Park-in</option>
-          <option value="cityLodge">City Lodge</option>
-        </select>
-      </div>
-      <br>
-      <hr>
-      <br>
-        <button type="submit">Submit</button>
-    </form>
-  </body>
-</html>
