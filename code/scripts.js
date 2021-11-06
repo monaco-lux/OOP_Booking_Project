771a3fd2b1bf947d0df07bec4c1fa8c745c5f7a5
Brendon de Beer <91954705+monaco-lux@users.noreply.github.com>
2021-11-06 20:29:56 +0200

    Added JS code to govern dates

---
+++ code/scripts.js
@@ -0,0 +1,21 @@
+var currentDateTime = new Date();
+var year = currentDateTime.getFullYear();
+var month = (currentDateTime.getMonth() + 1);
+var date = (currentDateTime.getDate() + 1);
+
+if(date < 10) {
+  date = '0' + date;
+}
+if(month < 10) {
+  month = '0' + month;
+}
+
+var dateTomorrow = year + "-" + month + "-" + date;
+var checkinElem = document.querySelector("#checkIn");
+var checkoutElem = document.querySelector("#checkOut");
+
+checkinElem.setAttribute("min", dateTomorrow);
+
+checkinElem.onchange = function () {
+    checkoutElem.setAttribute("min", this.value);
+}
